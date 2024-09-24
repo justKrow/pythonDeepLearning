1cf07524e13554ae530863d5e24be01c09fed762
justKrow <krowxen2214@gmail.com>
2024-09-24 21:10:13 +0100

    learn lambda

---
+++ functions/lambdas.py
@@ -0,0 +1,18 @@
+def a(x): return x + 1
+
+
+def simple_calc(a, b): return a + b
+
+
+print(a(10))
+
+# exercise
+# create a lambda function that accepts 1 integer argument
+# if the integer is > 5 return hello
+# otherwise return bye
+
+
+def x(x): return 'hello' if x > 5 else 'bye'
+
+
+print(x(10))
